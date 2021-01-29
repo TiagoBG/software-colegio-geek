f9ef299bbf176fd41a9a7034f80536a0d951083b
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-01-29 17:32:47 -0500

    Validacion de rol

--- BACKEND/routes/login.js
+++ BACKEND/routes/login.js
@@ -1,12 +1,12 @@
 const {Router} = require('express');
-const {cnn_mysql}=require('../config/database')
+const {cnn_mysql}=require('../config/database');
 const router = Router();
 
 
 router.post("/", (req, res) => {
-    const { correo, contrasena } = req.body;
+    const { correo, contrasena, rol } = req.body;
     cnn_mysql.query(
-      `SELECT * FROM usuario WHERE correo='${correo}' AND contrasena='${contrasena}'`,
+      `SELECT * FROM usuario WHERE correo='${correo}' AND contrasena='${contrasena}' AND rol='${rol}'`,
       (err, resulset, fields) => {
         if (err) {
           res.json({ message: `Error` });
