bf749fe55884ab506b627047082ecbfcdbe67e49
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-02-01 00:01:00 -0500

    Trabajando en la página de registrar notas Feb 1 0am

--- FRONTEND/src/components/SeeGrades.jsx
+++ FRONTEND/src/components/SeeGrades.jsx
@@ -6,6 +6,8 @@ import {getFromLocal} from '../functions/localStorage';
 
 export default function FollowUp() {
     const [infoUsuario, setInfoUsuario] = useState([]);
+    const subject = getFromLocal("subject");
+    const group = getFromLocal("group");
 
     useEffect(() => {
         obtenerMateriasUsuario();
@@ -14,18 +16,19 @@ export default function FollowUp() {
       function obtenerMateriasUsuario() {
         const id = getFromLocal("id");
         if (id) {
-          axios.get(`http://localhost:8083/seguimiento/${id}`).then(
+          axios.get(`http://localhost:8083/ver-notas/`).then(
             (res) => {
               setInfoUsuario(res.data);
             }
           );
         }
+        console.log(infoUsuario)
       }
     return (
         <section className="container-fluid w-100">
             <Card className='mx-auto my-5 p-5' style={{ width: '75vw' }}>
                 <div className='mx-auto mb-4'>
-                    <h3 className="text-center">Notas de #Grupo</h3>                    
+                    <h3 className="text-center">Notas {subject} de {group}</h3>                    
                 </div>
                 <div className='mx-auto'>
                 <a href="/admin" className='mx-4'><Button variant='info' className='mt-4 px-5'><b>Editar</b></Button></a>
@@ -37,7 +40,7 @@ export default function FollowUp() {
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">Materia</th>
+                                <th scope="col">Estudiante</th>
                                 <th scope="col">Seguimiento</th>
                                 <th scope="col">Autoevaluación</th>
                                 <th scope="col">Coevaluación</th>
@@ -46,17 +49,17 @@ export default function FollowUp() {
                             </tr>
                         </thead>
                         <tbody>
-                            {/* {infoUsuario.map((info)=>(
+                            {infoUsuario.map((info)=>(
                                 <tr key={info.id}>
                                     <td>{info.id}</td>
                                     <td>{info.nombre}</td>
                                     <td>{info.seguimiento}</td>
-                                    <td>{info.autoevaluacion}</td>
+                                    <td><input type='text'>{info.autoevaluacion}</input></td>
                                     <td>{info.coevaluacion}</td>
                                     <td>{info.evaluacion_periodo}</td>
                                     <td>{(info.autoevaluacion+info.coevaluacion+info.evaluacion_periodo)/3}</td>
                                 </tr>
-                                ))}   */}                          
+                                ))}                      
                         </tbody>
                     </table>
                 </div>                
